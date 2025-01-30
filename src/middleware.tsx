1d18af60aaa96e32cba10850081a754c16e30f74
metazfinance <127779614+metazfinance@users.noreply.github.com>
2025-01-30 13:19:40 +0700

    Update middleware.tsx

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,7 +3,7 @@ import { NextResponse, NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   if (pathname == "/") {
-    return NextResponse.redirect(new URL("/validator", req.url));
+    return NextResponse.redirect(new URL("/token", req.url));
   }
 
   return NextResponse.next();
